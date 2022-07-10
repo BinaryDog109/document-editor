cd0b3e64eb49d750f2fd0a5135936174b8079a3a
BinaryDog109 <emperoryty@outlook.com>
2022-07-10 09:34:46 +0100

    Added ctr;+ functionality

--- src/component/TextEditor.js
+++ src/component/TextEditor.js
@@ -1,24 +1,46 @@
 // Import the Slate editor factory.
-import styles from "./TextEditor.module.css"
+import styles from "./TextEditor.module.css";
 import { useCallback, useMemo, useState } from "react";
 import { createEditor } from "slate";
 // Import the `Editor` and `Transforms` helpers from Slate.
-import { Editor, Transforms } from 'slate'
+import { Editor, Transforms, Text } from "slate";
 
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from "slate-react";
 
- 
 import { useRenderElement } from "../hooks/useRenderElement";
 
-export const TextEditor = ({document, onChange}) => {
-    const editor = useMemo(() => withReact(createEditor()), []);
-    const { renderElement, renderLeaf } = useRenderElement(editor)
+export const TextEditor = ({ document, onChange }) => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const { renderElement, renderLeaf } = useRenderElement(editor);
 
-  return <div className={styles["editable-container"]}>
-    {/* We use onChange so that our document will also change */}
-    <Slate editor={editor} value={document} onChange={onChange}>
-      <Editable renderElement={renderElement} renderLeaf={renderLeaf} />
-    </Slate>
-  </div>
+  return (
+    <div className={styles["editable-container"]}>
+      {/* We use onChange so that our document will also change */}
+      <Slate editor={editor} value={document} onChange={onChange}>
+        <Editable
+          renderElement={renderElement}
+          renderLeaf={renderLeaf}
+          onKeyDown={(event) => {
+            if (event.key === "`" && event.ctrlKey) {
+              event.preventDefault();
+              // It will traverse from root to the current node you are at, like a tree, and return the generator
+              // (in terms of [[node1, postiion], [node2, position2], ...])
+              // The match option will traverse each node (node1, node2, ...).
+              // We will check if the nodes along the path are text nodes (leaves) and has .code to be true
+              const generator = Editor.nodes(editor, {
+                match: (n) => Text.isText(n) && n.code
+              });
+              const matchExists = !generator.next().done;
+              Transforms.setNodes(
+                editor,
+                { code: matchExists ? false : true },
+                { match: (n) => Text.isText(n), split: true }
+              );
+            }
+          }}
+        />
+      </Slate>
+    </div>
+  );
 };
